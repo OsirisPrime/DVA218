0ad68f4574ae25c79be9a1676d1460d6fa543dfc
Osiris Prime <132836318+OsirisPrime@users.noreply.github.com>
2024-05-09 22:22:36 +0200

    Update GBN.h

--- GBN.h
+++ GBN.h
@@ -14,7 +14,12 @@
 #include <sys/time.h>
 
 
-/*Packet/State types*/
+#define hostNameLength 50
+#define windowSize 1
+#define MAXMSG 1024
+
+
+/* Packet flags */
 #define SYN 0                  
 #define SYNACK 1
 #define DATA 2
@@ -23,10 +28,31 @@
 #define FINACK 5
 
 
-/*Transport protocol header*/
+int s_state;
+int r_state;
+
+/* Sender connection states */
+#define CLOSED 0
+#define WAIT_SYNACK 1
+#define RCVD_SYNACK 2
+#define ESTABLISHED 3
+
+/* Reciever connection states */
+#define LISTENING 0
+#define SENT_SYNACK 1
+#define WAIT_ACK 2
+#define ESTABLISHED 3
+
+/* Other states */
+#define WAIT_ACK 1
+#define WAIT_FIN 2
+#define WAIT_FINACK 3
+
+
+/* ransport protocol header */
 typedef struct rtp_struct {
     int flags;
-    int id;
+    //int id; If there are more then one connections
     int seq;
     int windowsize;
     int checksum;
@@ -34,10 +60,12 @@ typedef struct rtp_struct {
 } rtp;
 
 
-int threeWayHandshake(int sock, struct sockaddr_in serverName);
+int sender_connection(int sockfd, const struct sockaddr *serverName);
+int receiver_connection(int sockfd);
+
 int teardown(int sock, struct sockaddr_in serverName);
 
-int checksum(uint16_t *buffer, int len);
+uint16_t checksum(rtp *packet);
 int validate_checksum(rtp *packet);
 
 #endif
