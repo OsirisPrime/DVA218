d1978502b32faf876af958dbd21c2b230e91b35c
Osiris Prime <132836318+OsirisPrime@users.noreply.github.com>
2024-05-16 21:13:45 +0200

    Update GBN.h

--- GBN.h
+++ GBN.h
@@ -1,7 +1,11 @@
-/* File: GBN.h
- * Authors: Kim Svedberg, Zebastian Thorsén 
- * Description: File containing protocol varibles and states.
- */
+/*
+==========================================================================
+File            : gbn.h
+Authurs         : Kim Svedberg & Zebastian Thorsén 
+Version         : 1.0
+Description     : Header file for RT-protocol on-top of UDP
+==========================================================================
+*/
 
 #ifndef gbn_h
 #define gbn_h
@@ -13,22 +17,30 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 
-/* Protocal parameters */
-#define hostNameLength 50       /* The lenght of host name*/
-#define windowSize 1            /* Sliding window size */
-#define MAXMSG 1024             /* Maximun data to be sent once*/
-#define LOSS_PROB 0             /* Packet loss probability */
-#define CORR_PROB 0             /* Packet corrution probability */
-#define MAX_SEQ 100             /* The maximum random sequence number */
-#define MIN_SEQ 5               /* The minimum random sequence number */
+/*----- Protocal parameters -----*/
+#define PORT 5555                   /* Socket number */
+#define hostNameLength 50           /* The lenght of host name*/
+#define windowSize 1                /* Sliding window size */
+#define MAXMSG 1024                 /* Maximun data to be sent once*/
+#define LOSS_PROB 0                 /* Packet loss probability */
+#define CORR_PROB 0                 /* Packet corrution probability */
+#define MAX_SEQ 100                 /* The maximum random sequence number */
+#define MIN_SEQ 5                   /* The minimum random sequence number */
+#define TIMEOUT 5                   /* Timeout time (5s) */
 
-/* Packet flags */
+
+/*----- Packet flags -----*/
 #define SYN 0                  
 #define SYNACK 1
 #define DATA 2
@@ -36,51 +48,55 @@
 #define FIN 4
 #define FINACK 5
 
-int s_state;                    /* Sender state */
-int r_state;                    /* Receiver state */
 
-/* All possible states */
+/*----- All possible states -----*/
 enum states {
     CLOSED,
     WAIT_SYNACK,
-    RCVD_SYNACK,
     ESTABLISHED,
-    WAIT,
+    WAIT_OPEN,
+    WAIT_ACK,
     SEND_DATA,
     PACKET_LOSS,
-    RCVD_ACK,
     WAIT_FINACK,
-    RCVD_FINACK,
     WAIT_TIME,
     LISTENING,
-    RCVD_SYN,
-    WAIT_ACK,
+    WAIT_CLOSE,
+    WAIT_OPEN_ACK,
+    WAIT_CLOSE_ACK,
     READ_DATA,
-    DEFAULT,
-    RCVD_FIN,
 };
 
-/* Transport protocol header */
+
+/*----- Transport protocol header -----*/
 typedef struct rtp_struct {
-    uint8_t flags;
-    //int id; If there are more then one connections
-    uint8_t seq;
-    int windowsize;
-    uint16_t checksum;
-    uint8_t  data[MAXMSG];
+    int flags;
+    int seq;
+    int id;
+    int winSize;
+    int checksum;
+    char data[MAXMSG];
 } rtp;
 
-/* State information */
+
+/*----- State information -----*/
 typedef struct states_t{
     int state;
     int seqnum;
     int window_size;
+    int sockfd;
+    struct sockaddr *DestName;
+    struct sockaddr *OwnName;
+    socklen_t socklen;
+
 } state_t;
 
 
-/* All function for the protocol */
-int sender_connection(int sockfd, const struct sockaddr *serverName, socklen_t socklen);
-int receiver_connection(int sockfd, const struct sockaddr *client, socklen_t *socklen);
+/*----- All function for the protocol -----*/
+void gbn_init();
+int get_randNum();
+int sender_connect(int sockfd, const struct sockaddr *serverName, socklen_t socklen);
+int receiver_connect(int sockfd, const struct sockaddr *client, socklen_t *socklen);
 
 ssize_t sender_gbn(int sockfd, const void *buf, size_t len, int flags);
 ssize_t receiver_gbn(int sockfd, void *buf, size_t len, int flags);
@@ -90,6 +106,9 @@ ssize_t maybe_sendto(int sockfd, const void *buf, size_t len, int flags,
 int sender_teardown(int sockfd, const struct sockaddr *serverName, socklen_t socklen);
 int receiver_teardown(int sockfd, const struct sockaddr *client, socklen_t socklen);
 
-uint16_t checksum(rtp *packet);
+rtp packetBuild(rtp packet, int type);
+void signal_handler(int signum);
+unsigned short checksum(const rtp *packet);
+int verify_checksum(const rtp *packet);
 
 #endif
