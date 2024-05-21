ca98004ddeaca6a7d225bf53258ad5d380cbff90
Osiris Prime <132836318+OsirisPrime@users.noreply.github.com>
2024-05-21 13:56:26 +0200

    Update server.c

--- server.c
+++ server.c
@@ -3,6 +3,7 @@
 File Name       : server.c
 Authurs         : Kim Svedberg & Zebastian Thorsén 
 Version         : 1.0
+Date            : 2024-05-21
 Description     : A reliable GBN transfer protocol built on top of UDP.
                   Contains code for the receiver/server side.
 ===============================================================================
